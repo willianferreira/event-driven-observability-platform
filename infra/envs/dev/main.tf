f280d411f0ece86e9bd042e4174b3521fc955ac6
Willian Pinho <willianforbusiness@gmail.com>
2026-02-26 18:42:01 -0500

    infra: add lambda execution role with minimal sqs permissions

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -67,3 +67,55 @@ resource "aws_sqs_queue" "events" {
         ManagedBy = "terraform"
     }
 }
+
+resource "aws_iam_role" "lambda_role" {
+    name = "${var.project_name}-lambda-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "lambda.amazonaws.com"
+                }
+            }
+        ]
+    })
+
+    tags = {
+        Project = var.project_name
+        Environment = "dev"
+        ManagedBy = "terraform"
+    }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+    role       = aws_iam_role.lambda_role.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "lambda_sqs_policy" {
+    name = "${var.project_name}-lambda-sqs-policy"
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = [
+                    "sqs:ReceiveMessage",
+                    "sqs:DeleteMessage",
+                    "sqs:GetQueueAttributes"
+                ]
+                Effect   = "Allow"
+                Resource = aws_sqs_queue.events.arn
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach_sqs" {
+    role       = aws_iam_role.lambda_role.name
+    policy_arn = aws_iam_policy.lambda_sqs_policy.arn
+}
